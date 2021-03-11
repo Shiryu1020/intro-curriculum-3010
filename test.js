2ec1a82b39f735e2f6fbece68fbb146ae6b9742c
Shiryu <okymshru.1020@icloud.com>
2021-03-11 11:14:52 +0000

    tasks.jsonファイルを削除してからテストを実行するように修正

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('assert');
+
+const fs = require('fs');
+fs.unlink('./tasks.json', err => {
+  // テスト処理
+});
+
+const todo = require('./index.js');
 
 // add と list のテスト
 todo.add('ノートを買う');
